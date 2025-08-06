5ce71e445b01e3d03888a9ad2cac3ac24b2634ea
vkuznets23 <victoria.cuzneczowa23@gmail.com>
2025-08-06 12:04:07 +0300

    fixed modal

--- ping-pong/src/components/game.tsx
+++ ping-pong/src/components/game.tsx
@@ -41,12 +41,20 @@ const RoundResultModal = ({ winner, onNextRound }: RoundResultModalProps) => {
       className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-70 z-50"
       style={{ backdropFilter: 'blur(4px)' }}
     >
-      <div className="bg-gray-900 border-4 border-white-400 rounded-lg p-6 w-80 text-center">
-        <p className="text-white mb-6 text-lg">Winner: {winner}</p>
-        <img src={winnerImage} alt="winner" />
+      <div className="bg-gray-900 border-4 border-white rounded-lg p-8 w-96 text-center text-white space-y-6">
+        <h2 className="w-full text-center text-2xl font-bold mb-4">
+          🏁 Round winner: {winner}
+        </h2>
+        <div className="flex justify-center">
+          <img
+            src={winnerImage}
+            alt="winner"
+            className="w-24 h-24 object-cover"
+          />
+        </div>
         <button
           onClick={onNextRound}
-          className="bg-yellow-400 text-gray-900 font-semibold px-4 py-2 rounded hover:bg-yellow-300 transition"
+          className="w-full bg-yellow-400 text-gray-900 font-semibold px-4 py-2 rounded hover:bg-yellow-300 transition"
         >
           Next round
         </button>
